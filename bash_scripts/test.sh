08df3c69c378c4c2c46e28953b2a22c8fbd3cf7e
SebastianMerino <109495963+SebastianMerino@users.noreply.github.com>
2025-01-29 15:17:20 -0500

    script to run in computer cluster

---
+++ bash_scripts/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+#SBATCH --gpus-per-node=1
+#SBATCH --nodes=1
+#SBATCH --nodelist=worker10
+#SBATCH --partition=thinkstation-p360
+#SBATCH --output="slurm-%j.out"
+
+. /etc/profile.d/modules.sh
+module load msma-fullwave/1.0
+export PYTHONPATH=/mnt/nfs/smerino/fullwave_python:$PYTHONPATH
+srun python /mnt/nfs/smerino/fullwave_python/examples/ex_plane_wave_linear_transducer_transabdominal_imaging_animation.py
+
